f5ce8d18f650a30231dec043dc7227016b5a2403
moosecoop <cooper@makhijani.us>
2017-05-29 16:50:57 -0700

    updated responses for play command

--- commands/Beta/play.js
+++ commands/Beta/play.js
@@ -2,12 +2,13 @@ const ytdl = require('ytdl-core');
 
 exports.run = (client, msg, [link]) => {
   const voiceChannel = msg.member.voiceChannel;
+  let message = msg.reply('Playing song');
   if (!voiceChannel) return msg.reply(`Please be in a voice channel first!`);
   voiceChannel.join()
     .then(connnection => {
       const stream = ytdl(link, { filter: 'audioonly' });
       const dispatcher = connnection.playStream(stream);
-      dispatcher.on('end', () => voiceChannel.leave());
+      dispatcher.on('end', () => message.edit(`Played song`));
     });
 };
 
