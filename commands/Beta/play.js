cd28beb241e8236c7ae93cfbe8afbcecb83547e7
moosecoop <cooper@makhijani.us>
2017-05-29 17:02:57 -0700

    added volume setting

--- commands/Beta/play.js
+++ commands/Beta/play.js
@@ -1,14 +1,19 @@
 const ytdl = require('ytdl-core');
 
-exports.run = (client, msg, [link]) => {
+exports.run = (client, msg, [link, volume]) => {
   const voiceChannel = msg.member.voiceChannel;
+  let options = {
+    seek: 0,
+    volume: volume,
+    passes: 3
+  };
   
   if (!voiceChannel) return msg.reply(`Please be in a voice channel first!`);
   voiceChannel.join()
     .then(connnection => {
       let message = msg.reply('Playing song');
       const stream = ytdl(link, { filter: 'audioonly' });
-      const dispatcher = connnection.playStream(stream);
+      const dispatcher = connnection.playStream(stream, options);
       dispatcher.on('end', () => message.edit(`Played song`));
     });
 };
@@ -25,7 +30,7 @@ exports.conf = {
 exports.help = {
   name: "play",
   description: "plays a youtube video's audio (requires yt video link)",
-  usage: "<str:link>",
-  usageDelim: "",
+  usage: "<str:link>, [int:volume]",
+  usageDelim: "volume must be somewhere between 0 and 1",
 };
 
