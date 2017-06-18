d17f04a91672c66d721909d2425d88d4ab8ef729
moosecoop <cooper@makhijani.us>
2017-06-17 20:57:11 -0700

    updates

---
+++ commands/Beta/btc.js
@@ -0,0 +1,43 @@
+const coinTicker = require('coin-ticker');
+const Discord = require('discord.js');
+
+exports.run = (c, message) => {
+  var embed = new Discord.RichEmbed()
+      embed.setTitle('Bitcoin')
+      embed.setThumbnail('https://bitcoin.org/img/icons/opengraph.png')
+      embed.setTimestamp()
+      embed.setAuthor('MooseBot');
+  let Ttick = {};
+  coinTicker('bitfinex', [ 'btcusd']) // when no asset pair is specified, coinTicker will default to 'btcusd'
+.then( tick => {
+  console.log(tick)
+  embed.addField('Worth', `$${Ttick.ask}USD`)
+  Ttick = tick;
+})
+
+
+
+            message.channel.sendEmbed(embed);
+            var Client = require('coinbase').Client;
+var client = new Client({'apiKey': mykey, 'apiSecret': mysecret});
+
+            client.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, obj) {
+  console.log('total amount: ' + obj.data.amount);
+});
+  }
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text", "dm"],
+  selfbot: false,
+  aliases: ["currency", "crypto", "worth", "price"],
+  permLevel: 0,
+  botPerms: [],
+  requiredFuncs: [],
+};
+
+exports.help = {
+    name: 'btc',
+    description: 'gets the current value of a cryptocurrency',
+    usage: ''
+}
